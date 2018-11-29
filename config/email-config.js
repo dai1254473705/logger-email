2fc64099fb67fec613d5b8a1926fdd91e08ddbdc
daiyunzhou <daiyunzhou@zhugefang.com>
2018-11-29 10:59:52 +0800

    CHANGE CONFIG

--- config/email-config.js
+++ config/email-config.js
@@ -11,10 +11,9 @@ module.exports = {
     host: 'smtp.exmail.qq.com',
     port: 465,
     secure: true,// true for 465, false for other ports
-    user: 'daiyunzhou@zhuge.com', // generated ethereal user
-    pass: 'Dai123password', // generated ethereal password ！！！ 这是我的邮箱密码，各位大佬都是好人^_^
-    from: 'daiyunzhou@zhuge.com', // sender address
-    to: '18330822256@163.com,lixiaoyan@zhuge.com,wangyu1@zhuge.com,lijie@zhuge.com,dinghuihua@zhuge.com', // list of receivers 多个邮箱","分开
-    // to: '18330822256@163.com', // list of receivers 多个邮箱","分开
-    canSend: false,//是否可以发送邮件（false：不发送；true:发送）
+    user: 'your email', // generated ethereal user
+    pass: 'your password', // generated ethereal password ！！！ 这是我的邮箱密码，各位大佬都是好人^_^
+    from: '', // sender address
+    to: '@.com,@.com', // list of receivers 多个邮箱","分开
+    canSend: true,//是否可以发送邮件（false：不发送；true:发送）
 };
